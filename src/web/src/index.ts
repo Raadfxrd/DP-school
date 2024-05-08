5ab47cc0cc6b692095f9640588b6d1d197e7f49c
Raadfxrd <borysbabas@gmail.com>
2024-05-08 16:30:29 +0200

    Added GamesPage and MerchandisePage for later use

--- src/web/src/index.ts
+++ src/web/src/index.ts
@@ -1,3 +1,5 @@
 // NOTE: This makes sure the component can be rendered by the browser
 export * from "./components/Root";
 export * from "./components/ProductPage";
+export * from "./components/GamesPage";
+export * from "./components/MerchandisePage";
