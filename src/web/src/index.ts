4ba2d868e8b9037c9fa929c7731f21bbf71896aa
Pascal Togneri <pascal.togneri.togneri@hva.nl>
2024-05-17 08:19:18 +0200

    Documentatie + Cartpage

--- src/web/src/index.ts
+++ src/web/src/index.ts
@@ -1,8 +1,15 @@
 // NOTE: This makes sure the component can be rendered by the browser
 export * from "./components/Root";
 export * from "./components/ProductPage";
+<<<<<<< HEAD
 export * from "./components/CartPage";
+=======
+<<<<<<< HEAD
+>>>>>>> 9f041d4 (Documentatie + Cartpage)
 export * from "./components/GamesPage";
 export * from "./components/MerchandisePage";
 
 // export * from "./components/CartPage";
+=======
+export * from "./components/CartPage";
+>>>>>>> 631d1ec (Documentatie + Cartpage)
