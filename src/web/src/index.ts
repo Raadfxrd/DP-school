154132645331b410d7d64fe555eaa6876da3c643
Pascal Togneri <pascal.togneri.togneri@hva.nl>
2024-05-23 16:24:38 +0200

    QCKFIX

--- src/web/src/index.ts
+++ src/web/src/index.ts
@@ -1,5 +1,6 @@
 // NOTE: This makes sure the component can be rendered by the browser
 export * from "./components/Root";
 export * from "./components/ProductPage";
-export * from "./components/CartPage";export * from "./components/GamesPage";
+export * from "./components/CartPage";
+export * from "./components/GamesPage";
 export * from "./components/MerchandisePage";
